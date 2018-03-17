e5991023e97119bcb222f9d66958bcd9da48ce82
Samita Maharjan <samitamaharjan@gmail.com>
2018-03-17 13:54:12 -0700

    cleanup-kafka consumer

---
+++ src/main/scala/KafkaConsumer.scala
@@ -0,0 +1,32 @@
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010._
+
+object KafkaConsumer {
+
+  def main(args : Array[String]): Unit = {
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "1",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+    val conf = new SparkConf().setMaster("local[1]").setAppName("KafkaConsumer")
+    val ssc = new StreamingContext(conf, Seconds(15))
+    val topics = Array("test")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    stream.map(record => (record.value)).print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
